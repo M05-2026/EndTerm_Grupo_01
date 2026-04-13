2540936a0d722a81815f34c3edb563c2b9c40577
TDRaul <rteruel31@gmail.com>
2026-04-13 21:16:31 +0200

    Creación clase frenar

--- vehiculo-endterm/src/main/java/com/grupo/vehiculo/Vehiculo.java
+++ vehiculo-endterm/src/main/java/com/grupo/vehiculo/Vehiculo.java
@@ -8,7 +8,7 @@ public class Vehiculo {
     private int velocidadActual;
     private int velocidadMaxima;
 
-    public Vehiculo(String marca, String modelo, String matricula, String color, double precio) {
+    public Vehiculo(String marca, String modelo, String matricula, String color, double precio, int velocidadMaxima) {
         this.marca = marca;
         this.modelo = modelo;
         this.matricula = matricula;
@@ -16,5 +16,24 @@ public class Vehiculo {
         this.precio = precio;
         this.encendido = false;
         this.velocidadActual = 0;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public Acelerar(){
+
+        if (!encendido) {
+            System.out.println("El vehículo está apagado. No se puede acelerar.");
+            return;
+        }
+
+        int nuevaVelocidad = velocidadActual + incremento;
+
+        if (nuevaVelocidad > velocidadMaxima) {
+            velocidadActual = velocidadMaxima;
+            System.out.println("Velocidad máxima: " + velocidadMaxima + " km/h");
+        } else {
+            velocidadActual = nuevaVelocidad;
+            System.out.println("Velocidad actual: " + velocidadActual + " km/h");
+        }
     }
 }
